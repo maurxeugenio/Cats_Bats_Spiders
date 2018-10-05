b3c5df5722f356844882de889b905ff2fdebe0ce
Mauricio Eugênio <mauricio@digitalbluemarketing.com.br>
2018-10-05 09:07:48 -0300

    add cats bats spiders in julia lang #hacktoberfest #braziliam #ciro12

--- scripts/cats_bats_spiders.jl
+++ scripts/cats_bats_spiders.jl
@@ -1,23 +1,7 @@
 #=
-Author @maurxeugenio
-Algoritmo de ordenação buble sort, feito em Julia [juja]
-[os comentários em muitas linhas é bem mais lindo que em python.]
+    Author @maurxeugenio | @eugeniomaurx
+
 =#
-
-# lista_desordenada = [10, 3, 4, 5, 6, 1, 4, 5,6, 30, 1, 500, 200]
-
-# function odernar(lista)
-#     for i in 1:length(lista)
-# 		for j in 1:(length(lista) - 1)
-# 			if lista[j] > lista[j+1]
-# 				aux = lista[j]
-# 				lista[j] = lista[j+1]
-# 				lista[j+1] = aux
-# 			end
-# 		end
-# 	end
-# 	return lista
-# end
 
 for i = 1:100
     if i % 3 == 0
@@ -31,5 +15,3 @@ for i = 1:100
     println(i)
 
 end
-
-# print(odernar(lista_desordenada))
